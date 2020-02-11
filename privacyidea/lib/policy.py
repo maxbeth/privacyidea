97866d3c00589bf5ffd3ab8868928cec00d3a792
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2020-02-11 11:55:07 +0100

    Fix pep8 issues.

--- privacyidea/lib/policy.py
+++ privacyidea/lib/policy.py
@@ -2434,4 +2434,3 @@ class Match(object):
                    client=client, action=action, adminrealm=adminrealm,
                    adminuser=adminuser, time=time,
                    sort_by_priority=sort_by_priority)
-
