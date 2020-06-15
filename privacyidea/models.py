d2468ca754edb47c0b22b70d30133dece44a9700
Henning Hollermann <hollermann@physik.rwth-aachen.de>
2020-06-15 08:32:14 +0200

    review 2

--- privacyidea/models.py
+++ privacyidea/models.py
@@ -2135,7 +2135,7 @@ class SMSGateway(MethodsMixin, db.Model):
         """
         res = {}
         for option in self.options:
-            if option.Type == "option" or option.Type is None:
+            if option.Type == "option" or not option.Type:
                 res[option.Key] = option.Value
         return res
 
